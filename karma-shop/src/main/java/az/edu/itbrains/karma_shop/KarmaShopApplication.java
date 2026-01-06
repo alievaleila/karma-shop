a45ab18e03c0836eaf314dbad7eacaa35bca758b
alievaleila <meliyeva452@gmail.com>
2026-01-06 17:47:14 +0400

    feat: initial setup with Karma template UI components

---
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/KarmaShopApplication.java
@@ -0,0 +1,13 @@
+package az.edu.itbrains.karma_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmaShopApplication.class, args);
+	}
+
+}
