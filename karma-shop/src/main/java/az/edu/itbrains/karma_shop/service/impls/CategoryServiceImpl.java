ef72c8f9417de16ee0ca532805bed0d1cca2cc25
alievaleila <meliyeva452@gmail.com>
2026-01-07 21:31:03 +0400

    feat: implement CategoryServiceImpl

---
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/impls/CategoryServiceImpl.java
@@ -0,0 +1,10 @@
+package az.edu.itbrains.karma_shop.service.impls;
+
+import az.edu.itbrains.karma_shop.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+}
