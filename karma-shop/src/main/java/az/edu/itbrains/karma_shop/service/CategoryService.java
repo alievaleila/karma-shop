c106bd0f94a36799d0da30f0061a972264af6689
alievaleila <meliyeva452@gmail.com>
2026-01-07 21:45:42 +0400

    feat: implement getAllCategories() in CategoryService

--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/CategoryService.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/CategoryService.java
@@ -1,4 +1,10 @@
 package az.edu.itbrains.karma_shop.service;
 
+import az.edu.itbrains.karma_shop.dto.category.CategoryDto;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    List<CategoryDto> getAllCategories();
 }
