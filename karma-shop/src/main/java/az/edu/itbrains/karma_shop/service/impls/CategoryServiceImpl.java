c35d6275c8002e1a264301e1a69d1cd1129dfc08
alievaleila <meliyeva452@gmail.com>
2026-01-07 21:54:32 +0400

    feat: implement findAll() in CategoryServiceImpl

--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/impls/CategoryServiceImpl.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/impls/CategoryServiceImpl.java
@@ -1,10 +1,27 @@
 package az.edu.itbrains.karma_shop.service.impls;
 
+import az.edu.itbrains.karma_shop.dto.category.CategoryDto;
+import az.edu.itbrains.karma_shop.repository.CategoryRepository;
 import az.edu.itbrains.karma_shop.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public List<CategoryDto> getAllCategories() {
+        List<CategoryDto> categoryDtoList = categoryRepository.findAll()
+                .stream()
+                .map(category -> modelMapper.map(category, CategoryDto.class)).collect(Collectors.toList());
+        return categoryDtoList;
+    }
 }
