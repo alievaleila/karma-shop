985162750f6946f0eecae2d4f0fe57337317c671
alievaleila <meliyeva452@gmail.com>
2026-01-07 20:52:11 +0400

    fix: resolve model conflicts and configure ModelMapper

--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/config/MapperConfig.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/config/MapperConfig.java
@@ -1,11 +1,13 @@
 package az.edu.itbrains.karma_shop.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfig {
 
+    @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/model/Category.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/model/Category.java
@@ -1,34 +1,33 @@
 package az.edu.itbrains.karma_shop.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "products")
-public class Product {
+@Table(name = "categories")
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-    private Double price;
-    private Double discountPrice;
-    private String imageUrl;
-    private String description;
 
-    @ManyToOne
-    private Category category;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
 }
