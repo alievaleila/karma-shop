99e05a5a632013626585fd4c571b7f869a0bb51d
alievaleila <meliyeva452@gmail.com>
2026-01-09 22:17:22 +0400

    feat: connect contact form to backend and implement save logic

--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/controller/ContactController.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/controller/ContactController.java
@@ -1,4 +1,21 @@
 package az.edu.itbrains.karma_shop.controller;
 
+import az.edu.itbrains.karma_shop.dto.contact.ContactDto;
+import az.edu.itbrains.karma_shop.service.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class ContactController {
+
+    private final ContactService contactService;
+
+    @PostMapping("/contact")
+    public String addContact(@ModelAttribute("contact") ContactDto contactDto) {
+        contactService.addContact(contactDto);
+        return "redirect:/contact";
+    }
 }
