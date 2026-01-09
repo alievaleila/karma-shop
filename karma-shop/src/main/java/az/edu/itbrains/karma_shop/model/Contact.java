88ffade0650ee6cabb486bdb243f61cded02cb5c
alievaleila <meliyeva452@gmail.com>
2026-01-09 21:19:03 +0400

    feat: add Contact entity for feedback

---
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/model/Contact.java
@@ -0,0 +1,28 @@
+package az.edu.itbrains.karma_shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String subject;
+    private String message;
+}
