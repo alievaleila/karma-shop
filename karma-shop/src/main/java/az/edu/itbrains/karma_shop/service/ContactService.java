3bb2fb48d4bd09e94f095f3823c6e2d9c2381ac9
alievaleila <meliyeva452@gmail.com>
2026-01-09 22:00:55 +0400

    feat: implement addContact() in ContactService and save to database

--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/ContactService.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/ContactService.java
@@ -1,4 +1,8 @@
 package az.edu.itbrains.karma_shop.service;
 
+import az.edu.itbrains.karma_shop.dto.contact.ContactDto;
+
 public interface ContactService {
+
+    void addContact(ContactDto contactDto);
 }
--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/impls/ContactServiceImpl.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/impls/ContactServiceImpl.java
@@ -1,5 +1,8 @@
 package az.edu.itbrains.karma_shop.service.impls;
 
+import az.edu.itbrains.karma_shop.dto.contact.ContactDto;
+import az.edu.itbrains.karma_shop.model.Contact;
+import az.edu.itbrains.karma_shop.repository.ContactRepository;
 import az.edu.itbrains.karma_shop.service.ContactService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService {
+
+    private final ContactRepository contactRepository;
+
+    @Override
+    public void addContact(ContactDto contactDto) {
+        Contact contact=new Contact();
+        contact.setName(contactDto.getName());
+        contact.setEmail(contactDto.getEmail());
+        contact.setSubject(contactDto.getSubject());
+        contact.setMessage(contactDto.getMessage());
+
+        contactRepository.save(contact);
+    }
 }
