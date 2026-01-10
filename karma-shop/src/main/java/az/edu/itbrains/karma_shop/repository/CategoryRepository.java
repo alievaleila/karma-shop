2ae2af0ebf5af5fe7bc5a536f6174e3465090d7c
alievaleila <meliyeva452@gmail.com>
2026-01-10 23:48:09 +0400

    feat: add CategoryRepository with hierarchical query support

--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/repository/CategoryRepository.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package az.edu.itbrains.karma_shop.repository;
 import az.edu.itbrains.karma_shop.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAllByParentIsNull();
 }
