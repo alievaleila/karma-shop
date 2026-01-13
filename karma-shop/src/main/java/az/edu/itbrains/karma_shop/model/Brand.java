0ef6536234b5bfb9c2c01203fadcb73c619e4ab6
alievaleila <meliyeva452@gmail.com>
2026-01-13 16:56:36 +0400

    feat(domain): add Brand and Color entities and relate them to Product

--- karma-shop/src/main/java/az/edu/itbrains/karma_shop/model/Brand.java
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/model/Brand.java
@@ -5,40 +5,29 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "products")
-public class Product {
+@Table(name = "brands")
+public class Brand {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-    private Double price;
-    private Double discountPrice;
-    private String imageUrl;
-    private String description;
 
-    @ManyToOne
-    private Category category;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "brand_id")
-    private Brand brand;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "color_id")
-    private Color color;
+    @OneToMany(mappedBy = "brand", fetch = FetchType.LAZY)
+    private List<Product> products = new ArrayList<>();
 }
