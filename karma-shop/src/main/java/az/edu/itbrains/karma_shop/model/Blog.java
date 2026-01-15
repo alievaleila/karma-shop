9bdacafbb466d29bc9ee8b276309509f5249dfe8
alievaleila <meliyeva452@gmail.com>
2026-01-15 21:30:48 +0400

    feat: created Blog model with necessary fields and relationships

---
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/model/Blog.java
@@ -0,0 +1,50 @@
+package az.edu.itbrains.karma_shop.model;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+    private String imageUrl;
+
+    @Column(name="author_name")
+    private String author;
+
+    @Column(name="publish_date")
+    private LocalDateTime publishDate;
+
+    @Column(name="views")
+    private Long views;
+
+    @Column(name="comments")
+    private Long comments;
+
+    @ElementCollection
+    @CollectionTable(name = "post_categories", joinColumns = @JoinColumn(name = "post_id"))
+    @Column(name = "category")
+    private List<String> categories = new ArrayList<>();
+
+}
