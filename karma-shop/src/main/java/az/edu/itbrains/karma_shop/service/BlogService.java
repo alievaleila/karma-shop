f630fd39acab7f8329543a0bfee384063c83d59e
alievaleila <meliyeva452@gmail.com>
2026-01-15 21:55:44 +0400

    feat: created BlogService for business logic and managing blog-related operations

---
+++ karma-shop/src/main/java/az/edu/itbrains/karma_shop/service/BlogService.java
@@ -0,0 +1,10 @@
+package az.edu.itbrains.karma_shop.service;
+
+import az.edu.itbrains.karma_shop.dto.blog.BlogDto;
+
+import java.util.List;
+
+public interface BlogService {
+
+    List<BlogDto> getAllBlogs();
+}
